00502d6d8b307567af8302708eaea7e8bc24de39
Gabriela <amarroquinr11@miumg.edu.gt>
2023-08-04 21:59:39 -0600

    Primer y ultimo commit

---
+++ src/main/java/com/example/parcialprogra2/StatsController.java
@@ -0,0 +1,2 @@
+package com.example.parcialprogra2;public class StatsController {
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.example.parcialprogra2 {
+    requires javafx.controls;
+    requires javafx.fxml;
+            
+            requires com.dlsc.formsfx;
+                        
+    opens com.example.parcialprogra2 to javafx.fxml;
+    exports com.example.parcialprogra2;
+}
